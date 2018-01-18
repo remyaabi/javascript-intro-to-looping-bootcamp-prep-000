8c5704861c01da0950515f730cb6bd8b8879fca9
Remya Jacob <remyaabi@gmail.com>
2018-01-18 22:00:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ var stringArray=[];
 var n=10;
 function forLoop(stringArray){
    for(var i=0; i<25; i++){
-       let s = i === 0 || 1 ? "I am ${i} strange loop." : `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
+       let s = i === 0 || 1 ? `I am ${i} strange loop.` : `I am ${i} strange loops.`
        stringArray.push(s);
    }
   
